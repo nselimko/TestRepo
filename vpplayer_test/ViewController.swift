a2bd9e3297a8e0f12b103e1bd95f074d037aa75e
Nuridin Selimko <nuridin@gjirafa.com>
2021-07-06 09:13:15 +0200

    Initial Commit

---
+++ vpplayer_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vpplayer_test
+//
+//  Created by Nuridin on 7/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
